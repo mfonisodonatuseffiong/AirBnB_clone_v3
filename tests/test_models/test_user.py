abaf2d0fa341a6c22bca3220e1b2c9f89b06a893
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-28 03:50:37 +0100

    0x02. AirBnB clone - MySQL

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,11 +6,10 @@ Contains the TestUserDocs classes
 from datetime import datetime
 import inspect
 import models
-from models import user
+from models.user import User
 from models.base_model import BaseModel
-import pep8
+import pycodestyle
 import unittest
-User = user.User
 
 
 class TestUserDocs(unittest.TestCase):
@@ -22,27 +21,27 @@ class TestUserDocs(unittest.TestCase):
 
     def test_pep8_conformance_user(self):
         """Test that models/user.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
+        pep8s = pycodestyle.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/user.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_user(self):
         """Test that tests/test_models/test_user.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
+        pep8s = pycodestyle.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_models/test_user.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_user_module_docstring(self):
         """Test for the user.py module docstring"""
-        self.assertIsNot(user.__doc__, None,
+        self.assertIsNot(models.user.__doc__, None,
                          "user.py needs a docstring")
-        self.assertTrue(len(user.__doc__) >= 1,
+        self.assertTrue(len(models.user.__doc__) >= 1,
                         "user.py needs a docstring")
 
     def test_user_class_docstring(self):
-        """Test for the City class docstring"""
+        """Test for the User class docstring"""
         self.assertIsNot(User.__doc__, None,
                          "User class needs a docstring")
         self.assertTrue(len(User.__doc__) >= 1,
@@ -110,7 +109,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in u.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
